5272612a3572e40ede4749ef6dc9d60bfc3acd43
Martin Machmar <mmachmar@scharfsteinlabs.com>
2020-10-13 15:34:10 -0300

    se agrego lineas de php

--- inicio.php
+++ inicio.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<<<<<<< HEAD
-    
-=======
-    <?php
-        echo "primera moficiacion";
-    ?>
->>>>>>> modificacion
-</body>
-</html>
+<?php
+    echo "solucionando conflicto";
+?>
